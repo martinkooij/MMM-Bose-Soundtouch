5926bef0d7ec69c0584121e7a8d83d63360286dd
martinkooij <martin.kooij.4@gmail.com>
2021-02-10 15:58:43 +0100

    make list of boses

--- node_helper.js
+++ node_helper.js
@@ -2,10 +2,11 @@
  * Module: MagicMirror-Bose-Module
  *
  * By SpoturDeal https://github.com/
+ * list enhancement & got rid of deprecated "request" dependency
  * MIT Licensed.
  */
 var NodeHelper = require('node_helper');
-var request = require('request');
+var getBose = require('node-fetch');
 
 module.exports = NodeHelper.create({
   start: function () {
@@ -15,14 +16,44 @@ module.exports = NodeHelper.create({
   socketNotificationReceived: function(notification, url) {
     if (notification === 'BOSE_READ') {
 //      console.log(notification, url);
-      var self = this;
-      request(url, function(error, response, body) {
-        if (!error && response.statusCode == 200) {
-//		  console.log(body);
-		  self.sendSocketNotification('BOSE_DATA', body);
+		const result = readAllboses(url);
+        if (result.res != "error") {
+//		  console.log(result.body);
+		  self.sendSocketNotification('BOSE_DATA', result.body);
 		}
       });
     }
+  },
+  
+  readOnebose: async function(endpoint) {
+	const regexp = /source="INVALID_SOURCE"|source="STANDBY"/ ;
+	try {
+		const res = await getBose(endpoint) ;
+		const data = await res.text;
+		console.log(endpoint, "=" , data) ;
+		if (data.search(regexp) != -1) {
+			return {res: "empty" , body: data} ;
+		}
+		return {res: "full" , body: data} ;
+	} catch (err) {
+		return {res: "error" , body: err};
+	}
+  },
+	  
+  readAllboses: function (iplist) {
+  if (!Array.isArry(urllist)) { iplist = [iplist] } ;
+  var firstanswer ;
+  (async () => {
+	  var answer ;
+	  for (let ip in iplist) {
+		  console.log("ip = ", ip) ;
+		  answer = await readOnebose("http://"+ ip + ":8090/now_playing") ;
+		  if (answer.res === "full") { break} ;
+	  }
+	  firstanswer = answer ;
+  })() ;
+  return firstanswer ;
   }
+	 
   
 });
